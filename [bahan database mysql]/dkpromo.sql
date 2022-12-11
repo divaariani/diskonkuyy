fd3c9e1449ecb40203f8ed193c9e666945e52db9
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2022-12-11 11:51:56 +0700

    Add files via upload

---
+++ [bahan database mysql]/dkpromo.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2022 at 08:32 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `diskonkuyy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dkpromo`
+--
+
+CREATE TABLE `dkpromo` (
+  `id` int(11) NOT NULL,
+  `tempat` varchar(30) NOT NULL,
+  `promo` varchar(50) NOT NULL,
+  `bataspromo` date NOT NULL,
+  `harga_awal` int(11) NOT NULL,
+  `harga_akhir` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dkpromo`
+--
+ALTER TABLE `dkpromo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dkpromo`
+--
+ALTER TABLE `dkpromo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
